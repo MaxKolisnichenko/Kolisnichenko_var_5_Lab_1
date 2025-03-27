b8cdd21a2e1c42af91136f130c02cc1c0e8a5a51
MaxKolisnichenko <126777664+MaxKolisnichenko@users.noreply.github.com>
2025-03-27 15:03:17 +0200

    Lab_1.1.py

--- Lab_1.1.py
+++ Lab_1.1.py
@@ -1,11 +1,24 @@
 #Лаб.1 Рівень 1. Варіант 5.
 import math
 
-R = float(input("Введіть радіус більшої основи (R): "))
-r = float(input("Введіть радіус меншої основи (r): "))
-H = float(input("Введіть висоту (H): "))
+def is_number(value):
+    try:
+        return float(value)
+    except ValueError:
+        return None
 
-l = math.sqrt((R - r) ** 2 + H ** 2) # обчислення твірної
-S = math.pi * (R**2 + r**2 + l * (R + r)) # обчислення площі повної поверхні
+R = is_number(input("Введіть радіус більшої основи (R): "))
+r = is_number(input("Введіть радіус меншої основи (r): "))
+H = is_number(input("Введіть висоту (H): "))
 
-print(f"Площа повної поверхні зрізаного конуса: {S:.3f}")
+if R is None or r is None or H is None:
+    print("Помилка: Введені значення повинні бути числами.")
+elif R <= 0 or r <= 0 or H <= 0:
+    print("Помилка: усі значення повинні бути додатними.")
+elif R < r:
+    print("Помилка: R має бути більшим або рівним r.")
+else:
+    l = math.sqrt((R - r) ** 2 + H ** 2)  # - обчислення твірної
+    S = math.pi * (R**2 + r**2 + l * (R + r))  # - обчислення площі повної поверхні
+    print(f"Площа повної поверхні зрізаного конуса: {S:.3f}")
+
