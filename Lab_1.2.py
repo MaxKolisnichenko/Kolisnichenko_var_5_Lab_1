1d920832551eca5cdad13df72351f67259e811b4
MaxKolisnichenko <126777664+MaxKolisnichenko@users.noreply.github.com>
2025-03-27 15:03:40 +0200

    Lab_1.2.py

--- Lab_1.2.py
+++ Lab_1.2.py
@@ -1,16 +1,25 @@
 #Лаб.1 Рівень 2. Варіант 5.
 
-a = int(input("Введіть довжину сторони a: "))
-b = int(input("Введіть довжину сторони b: "))
-c = int(input("Введіть довжину сторони c: "))
+def is_number(value):
+    try:
+        return float(value)
+    except ValueError:
+        return None
 
-if a <= 0 or b <= 0 or c <= 0:
-    print("Сторони трикутника повинні бути більше 0")
+a = is_number(input("Введіть довжину сторони a: "))
+b = is_number(input("Введіть довжину сторони b: "))
+c = is_number(input("Введіть довжину сторони c: "))
 
-if a + b > c and a + c > b and b + c > a:
+if a is None or b is None or c is None:
+    print("Помилка: Введені значення повинні бути числами.")
+elif a <= 0 or b <= 0 or c <= 0:
+    print("Помилка: Сторони трикутника повинні бути більше 0.")
+elif a + b <= c or a + c <= b or b + c <= a:
+    print("Помилка: Трикутника з такими сторонами не існує.")
+else:
     if a == b == c:
-        print(1)  # - рівносторонній
+        print(1)  # рівносторонній
     elif a == b or a == c or b == c:
-        print(2)  # - рівнобедрений
+        print(2)  # рівнобедрений
     else:
-        print(3)  # - різносторонній
+        print(3)  # різносторонній
